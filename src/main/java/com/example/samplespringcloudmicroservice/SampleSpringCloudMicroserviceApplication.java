19b4d1f6f2ccef44998f3f56c2d36418572ff9d7
ghulsure <91338772+ghulsure@users.noreply.github.com>
2022-11-28 21:22:29 -0500

    Add files via upload

---
+++ src/main/java/com/example/samplespringcloudmicroservice/SampleSpringCloudMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplespringcloudmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringCloudMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringCloudMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samplespringcloudmicroservice/SampleSpringCloudMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplespringcloudmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringCloudMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
